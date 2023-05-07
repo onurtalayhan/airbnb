eef5b34bab7c44a8414215b839172ea9d4f28832
onurtalayhan <onur.talayhan@gmail.com>
2023-05-07 19:51:51 +0200

    working with clientonly

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+'use client';
+import { children, useEffect, useState } from "react";
 
 const ClientOnly = () => {
+    children
     const [hasMounted, sethasMounted] = useState(false);
-    return (  );
+    useEffect(() => {
+        sethasMounted(true);
+    }, [])
+    if (!hasMounted) {
+        return null;
+    }
+    return (
+        <>
+            {children}
+        </>
+    );
 }
 
 export default ClientOnly;
